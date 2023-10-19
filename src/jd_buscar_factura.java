ed0ce09ce481f2c7d86828a0bde142b31f7c8bb9
Wedelp <cristiancastrojimenez96@hotmail.com>
2023-10-19 13:05:41 +0200

    jd_buscar_factura
    Está casi hecho

--- src/jd_buscar_factura.java
+++ src/jd_buscar_factura.java
@@ -7,11 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class jd_buscar_factura extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtIntroduceTexto;
 
 	/**
 	 * Launch the application.
@@ -37,6 +40,58 @@ public class jd_buscar_factura extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		
+		JLabel lblDentilax = new JLabel("DENTILAX CLINICA ODONTOLOGICA");
+		lblDentilax.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblDentilax.setBounds(201, 26, 317, 31);
+		contentPanel.add(lblDentilax);
+		
+		JLabel lblNombre = new JLabel("NOMBRE");
+		lblNombre.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblNombre.setBounds(45, 89, 114, 31);
+		contentPanel.add(lblNombre);
+		
+		JLabel lblApellidos = new JLabel("APELLIDOS");
+		lblApellidos.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblApellidos.setBounds(45, 131, 114, 24);
+		contentPanel.add(lblApellidos);
+		
+		JLabel lblDni = new JLabel("DNI");
+		lblDni.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblDni.setBounds(45, 169, 60, 18);
+		contentPanel.add(lblDni);
+		
+		JLabel lblDireccion = new JLabel("DIRECCION");
+		lblDireccion.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblDireccion.setBounds(45, 196, 114, 37);
+		contentPanel.add(lblDireccion);
+		
+		JLabel lblTelefono = new JLabel("TELEFONO");
+		lblTelefono.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblTelefono.setBounds(45, 240, 114, 24);
+		contentPanel.add(lblTelefono);
+		
+		JLabel lblFecha = new JLabel("FECHA");
+		lblFecha.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblFecha.setBounds(45, 275, 114, 37);
+		contentPanel.add(lblFecha);
+		
+		JLabel lblPagado = new JLabel("PAGADO");
+		lblPagado.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblPagado.setBounds(45, 317, 114, 24);
+		contentPanel.add(lblPagado);
+		
+		JLabel lblPorPagar = new JLabel("POR PAGAR");
+		lblPorPagar.setFont(new Font("Barlow", Font.BOLD, 20));
+		lblPorPagar.setBounds(45, 352, 114, 31);
+		contentPanel.add(lblPorPagar);
+		
+		txtIntroduceTexto = new JTextField();
+		txtIntroduceTexto.setFont(new Font("Barlow", Font.BOLD, 20));
+		txtIntroduceTexto.setText("Introduce texto");
+		txtIntroduceTexto.setBounds(212, 94, 253, 20);
+		contentPanel.add(txtIntroduceTexto);
+		txtIntroduceTexto.setColumns(10);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setBackground(new Color(255, 255, 255));
@@ -57,5 +112,4 @@ public class jd_buscar_factura extends JDialog {
 			}
 		}
 	}
-
 }
